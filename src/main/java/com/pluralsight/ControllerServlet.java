791828356c46bad227471c59b9babe1a63bf5747
talmadge413 <talmadge413@yahoo.com>
2019-04-03 13:31:50 -0400

    Completed the module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -121,7 +121,7 @@ public class ControllerServlet extends HttpServlet {
 	
 	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
-			int id = Integer.parseInt(request.getParameter("did"));
+			int id = Integer.parseInt(request.getParameter("id"));
 			Book existingBook = bookDAO.getBook(id);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
 			request.setAttribute("book", existingBook);
